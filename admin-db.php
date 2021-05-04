a6d6e50d9156c0f97a6a7b1c45f24a09c05ac561
Pasindu Lakshan <74607482+pasindulakshan@users.noreply.github.com>
2021-05-04 21:36:15 +0530

    Admin Dashboard

--- admin-db.php
+++ admin-db.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html>
   <head>
+    <script src="javascript/charts.js"></script>
     <link rel="styleSheet" href="css/admin-db.css" />
     <link rel="styleSheet" href="css/admin-sidebar.css" />
     <?php include 'header.php'; ?>
@@ -10,16 +11,51 @@
     <div class="a-container">
       <?php include 'admin-sidebar.php'; ?>
       <div class="db-container">
-        <div class="innumbers">
-          <h3>Total Members:</h3>
-          <h3>Total Free Members:</h3>
-          <h3>Total Premium Members:</h3>
-          <h3>New Members Registerd in last 30 days:</h3>
-          <h3>New Free Members Registerd in last 30 days:</h3>
-          <h3>New Premium Members Registerd in last 30 days:</h3>
-          <h3>Total Income in last 30 days:</h3>
+        <div class="in-numbers">
+          <div class="in-numbers-item">
+            <h3>Total Members:</h3>
+            <p>110</p>
+          </div>
+          <div class="in-numbers-item">
+            <h3>Total Free Members:</h3>
+            <p>60</p>
+          </div>
+          <div class="in-numbers-item">
+            <h3>Total Premium Members:</h3>
+            <p>40</p>
+          </div>
+          <div class="in-numbers-item">
+            <h3>Total Overdue Members:</h3>
+            <p>10</p>
+          </div>
+          <div class="in-numbers-item">
+            <h3>New Members Registerd in last 30 days:</h3>
+            <p>50</p>
+          </div>
+          <div class="in-numbers-item">
+            <h3>New Free Members Registerd in last 30 days:</h3>
+            <p>30</p>
+          </div>
+          <div class="in-numbers-item">
+            <h3>New Premium Members Registerd in last 30 days:</h3>
+            <p>20</p>
+          </div>
+          <div class="in-numbers-item">
+            <h3>Total Income in last 30 days:</h3>
+            <p>400 USD</p>
+          </div>
         </div>
-        <div class="graphical"></div>
+        <div class="graphical">
+          <div class="p-chart">
+            <p>Members</p>
+          </div>
+          <div class="m-chart">
+            <p>Member Registrations - past 6 Months</p>
+          </div>
+          <div class="i-chart">
+            <p>income - past 6 months</p>
+          </div>
+        </div>
       </div>
     </div>
   </body>
