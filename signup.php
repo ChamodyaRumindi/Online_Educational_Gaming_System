8210e4ca1ca39c8d65614a1a06150ed8d69b472d
chamodya_rumindi <rumicham99@gmail.com>
2021-05-05 18:03:02 +0530

    Update signup.php

--- signup.php
+++ signup.php
@@ -4,6 +4,51 @@
 
 ?>
 
+<?php
+    if (isset($_POST["submit"])) {
+        /*echo "It works";*/
+
+        $name = $_POST["fname"];
+        $email = $_POST["email"];
+        $pwd = $_POST["pwd"];
+        $pwdRepeat = $_POST["pwdrepeat"];
+
+        require_once 'database.php';
+        require_once 'functions.php';
+
+        if (emptyInputSignup($name, $email, $pwd, $pwdRepeat) !== false) {
+            header ("location: ../signup.php?error=emptyinput");
+            exit();
+        }
+
+        if (invalidEmail($email) !== false) {
+            header ("location: ../signup.php?error=invalidemail");
+            exit();
+        }
+
+        if (pwdMatch($pwd, $pwdRepeat) !== false) {
+            header ("location: ../signup.php?error=passwordsdontmatch");
+            exit();
+        }
+
+        if (uidExists( $conn, $email) !== false) {
+            header ("location: ../signup.php?error=emailtaken");
+            exit();
+        }
+
+        createUser($conn, $name, $email, $pwd);
+
+
+    }
+
+    else {
+        header ("location: ../signup.php");
+        exit();
+    }
+
+
+?>
+
 
 <!DOCTYPE>
 
@@ -18,7 +63,7 @@
 
             <div class ="signup-form-form">
 
-                <form action ="signup.php" method ="POST">
+                <form action ="includes/signup.php" method ="POST">
                         <label><b>Full Name*</b></label>
                     <br><br>
                         <input type ="text" name ="fname" placeholder ="full name" required/>
