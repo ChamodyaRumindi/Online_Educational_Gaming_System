b5831615bab74b3c1bc427961ffe0604afa8d6bc
chamodya_rumindi <rumicham99@gmail.com>
2021-05-05 18:03:31 +0530

    Update database.php

--- database.php
+++ database.php
@@ -3,12 +3,12 @@
     $serverName = "localhost";
     $dBUserName = "root";
     $dBPassword = "";
-    $dBName = "oegplay";
+    $dBName = "oeg_play";
 
     $conn = mysqli_connect($serverName, $dBUserName, $dBPassword, $dBName );
 
     if ($conn){
-        /*die("connection failed: ". mysqli_connect_error());*/
-        echo "connected";
+        /*die("connection failed: ". mysqli_connect_error()); */
+        echo "connected";  
     }
 ?>
