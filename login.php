ce898a15c4c27c7f6d64a9a48f9f799ac1212b1a
chamodya_rumindi <rumicham99@gmail.com>
2021-05-05 11:31:05 +0530

    login page

---
+++ login.php
@@ -0,0 +1,72 @@
+<?php
+
+    include_once 'header.php';
+    
+?>
+
+<!DOCTYPE>
+<html>
+    <head>  
+        <link rel="styleSheet" href="css/login.css" />
+    </head>
+
+    <body>
+        <div class ="login"> 
+            <form id ="login" method ="POST" action ="login.php">  
+            <h2>SIGN IN</h2>
+            
+                <div class ="form-group">
+                    <label><b>E-mail</b></label><br>
+
+                    <input type ="email" name ="email" id ="email" placeholder="e-mail address" required autofocus/> 
+                    
+                    <br>
+                
+                </div>
+
+                <div class ="form-group">
+                    <label><b>Password</b></label><br>
+                    
+                    <input type ="password" name ="psw" id ="psw" placeholder="password" maxlength ="10" required/>
+
+                    <br>
+
+                </div>
+                
+                
+
+                <a class ="reset-psw" href ="#">Forgot Password?</a>
+                
+                <br><br>
+
+                <button type ="submit" class ="login-btn"  name ="log" id ="log">LOG IN</button>
+
+                
+            
+                <div class ="seperator"><b>Or</b><b>Sign</b><b>In</b><b>With</b></div>
+
+                <div class ="socialIcon">
+                    <a class ="btn1" href ="#">
+                        <img class ="social" src ="images/Facebook.png"/>
+                    </a>
+
+                    <a class ="btn1" href ="#">  
+                        <img class ="social" src ="images/Twitter.png"/>
+                    </a>
+
+                    <a class ="btn1" href ="#">  
+                        <img class ="social" src ="images/G-mail.png"/>
+                    </a>
+                </div>
+
+                
+
+            </form>
+        </div>
+    </body>
+    
+</html>
+
+<?php
+    include_once 'footer.php';
+?>
