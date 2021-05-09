ef7bb0cc72fa1cc8ec9498a398d7362cbfba84c1
nethu99 <it20632700@my.sliit.lk>
2021-05-09 10:09:34 +0530

    choose-subcription.php and payment.php page pushed git

---
+++ payment.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Payment Page</title>
+       <link rel="stylesheet" href="css/payment.css" />
+  </head>
+  <body>
+    <div class="container">
+      <div class="imgDiv">
+        <img src="images/payment-methods.png" alt="payment-picture" width="500" />
+      </div>
+      <div class="formDiv">
+        <form action="" method="#">
+          <label for="name">Name:</label>
+          <input type="text" name="userName" id="name" /><br />
+          <label for="email">Email:</label>
+          <input type="email" name="email" id="email" />
+          <br />
+          <p>Membership:Premium</p>
+          <p>Plan:Monthly Subcription</p>
+          <p>Due:M$19.00</p>
+          <p>Next Due date:M$19.00</p>
+          <button type="submit">Pay Now</button>
+        </form>
+      </div>
+    </div>
+  </body>
+</html>
