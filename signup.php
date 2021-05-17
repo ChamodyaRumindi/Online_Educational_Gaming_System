07a93b9325f7de81bd76d4c976b72a6e525a6062
chamodya_rumindi <rumicham99@gmail.com>
2021-05-17 16:30:26 +0530

    update signup.php

--- signup.php
+++ signup.php
@@ -14,33 +14,40 @@
 
     <body>
         <section class ="signup_form">
-            <h2>SIGN UP</h2>
+            
 
             <div class ="signup-form-form">
 
                 <form action ="includes/signup.inc.php" method ="POST">
-                        <label><b>Full Name*</b></label>
-                    <br><br>
+                     <h2>SIGN UP</h2>
+                        <label><b>Full Name</b></label>
+                    <br>
                         <input type ="text" name ="fname" placeholder ="full name" required/>
-                    <br><br>
-                        <label><b>E-mail*</b></label>
-                    <br><br>
+                    <br>
+                        <label><b>E-mail</b></label>
+                    <br>
 
                         <input type ="email" name ="email" placeholder ="e-mail" required/>
-                    <br><br>
-                        <label><b>Password* minimum 8 charactors</b></label>
+                    
+                    <br>
+                        <label><b>User Name</b></label>
+                    <br>
+                    
+                        <input type ="text" name ="username" placeholder ="user name" required/>
+                    <br>
+                        <label id ="character"><b>Password</b></label>
                         
-                    <br><br>
-                        <input type ="password" name ="pwd" placeholder ="password" required/>
-                    <br><br>
-                        <label><b>Confirm Password*</b></label>
-                    <br><br>
+                    <br>
+                        <input type ="password" name ="pwd" placeholder ="password" pattern ="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}" title ="Must contain at least one number and one uppercase and lowercase letter, and at least 8 or more characters" required/>
+                    <br>
+                        <label><b>Confirm Password</b></label>
+                    <br>
                        
                         <input type ="password" name ="pwdrepeat" placeholder ="confirm password" required/>
                     <br>
 
                         <p>by submitting this form you agree to our <a href = "#">terms of use</a></p>
-                    <br>    
+                       
                         <button type ="submit" name ="submit">SIGN UP</button>
 
 
