59e0f960660c21172c82ddd3486c01b57e9c0ab4
chamodya_rumindi <rumicham99@gmail.com>
2021-05-19 01:44:48 +0530

    modify login page

--- login.php
+++ login.php
@@ -7,33 +7,35 @@
 
 <!DOCTYPE>
 <html>
+<<<<<<< Updated upstream
     <head>
         <link rel="styleSheet" href="css/login.css" />
+=======
+    <head>  
+    <link rel="styleSheet" href="css/login.css" />
+>>>>>>> Stashed changes
     </head>
     <body>
-    <section>
-        <div class ="login"> 
-            <form id ="login" method ="POST" action ="includes/login.inc.php">  
-            <h2>SIGN IN</h2>
+    <section class ="login">
+        <div class ="form_group"> 
             
-                <div class ="form-group">
-                    <label><b>E-mail</b></label><br>
+            <form method ="POST" action ="includes/login.inc.php">  
+				<h2>SIGN IN</h2>
+            
+                
+                    <label><b>E-mail or User Name</b></label><br>
 
-                    <input type ="email" name ="email" id ="email" placeholder="e-mail address" required autofocus/> 
+                    <input type ="text" name ="username" id ="username" placeholder="e-mail address / user name" required /> 
                     
                     <br>
                 
-                </div>
-
-                <div class ="form-group">
                     <label><b>Password</b></label><br>
                     
                     <input type ="password" name ="pwd" id ="pwd" placeholder="password" maxlength ="10" required/>
 
-                    <br>
-
-                </div>
+                    <br>             
                 
+<<<<<<< Updated upstream
                 <div class="l-container">
                     <div class="checkbox">
                         <label><b>Remember ME</b></label>
@@ -41,12 +43,24 @@
                     </div>
                 <a class ="reset-psw" href ="#">Forgot Password?</a>
                 </div>
+=======
+				
+                <div class ="checkbox_container">
+                    <input type ="checkbox" name= "remember" id ="remember"/>
+					<span>Remember Me</span>
+
+                    <a class ="reset-psw" href ="#">Forgot Password?</a>
+                </div>
+                <br><br>
+>>>>>>> Stashed changes
 
                 <button type ="submit" class ="login-btn"  name ="log" id ="log">LOG IN</button>
 
                 
             
-                <div class ="seperator"><b>Or</b><b>Sign</b><b>In</b><b>With</b></div>
+                <hr>
+				
+				<p class ="or">Or Sign In With</p>
 
                 <div class ="socialIcon">
                     <a class ="btn1" href ="#">
@@ -61,7 +75,6 @@
                         <img class ="social" src ="images/G-mail.png"/>
                     </a>
                 </div>
-
                 
 
             </form>
