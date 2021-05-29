d71463a053bed455652cec4994fc9ed96c5dafbd
it20609962 <it20609962@my.sliit.lk>
2021-05-29 14:18:22 +0530

    settings pages

--- userSettings.php
+++ userSettings.php
@@ -6,40 +6,54 @@
         ?>
 
          <script language="javascript" type="text/javascript">
-         
+         function save()
+         {
+             getElementByClass("save-button");
+             alert("Saved!!!");
+         }
+         function delete()
+         {
+             getElementByClass("delete-button");
+             alert("Your account is successfully deleted!!");
+         }
          </script>
 
     <div class="st-container">
         <?php include "user-sidebar.php"; ?>
 
+       <div class="setting-form">
        <form class="form-details" method="POST">
           Change Profile Picture:
           <input type ="search" id="bsearch" name="bsearch">
           <input type="button" name="browse" value="Browse">
-          <br><br><br><br><br><br>
+          <br>
+          <img src="images/defaultProfile.png" class="st-image">
+          <br><br>
           
           <label for="name">Change Name:     </label>
-          <input type="text" id="name" name="name" value="Pasindu Lakshan"required>
+          <input type="text" class="fields" id="name" name="name" required>
           <br> <br>
 
           <label for="email">Change E-mail:    </label>
-          <input type="email" id="email" name="email" value="abcTest@gmail.com" required>
+          <input type="email" class="fields" id="email" name="email" placeholder="abcTest@gmail.com" required>
           <br><br>
 
           <label for="pwd">Change Password:    </label>
-          <input type="password" maxlength="8" id="pwd" name="pwd" placeholder="Current password">
+          <input type="password" class="fields" maxlength="50" id="pwd" name="pwd" placeholder="Current password">
           <br><br>
-          <input type="password" maxlength="8" id="pwd1" name="pwd1" placeholder="New password">
+          <input type="password" class="fields" maxlength="50" id="pwd1" name="pwd1" placeholder="New password">
+          <br><font size="1" color="red">(*minimum 8 characters)</font>
           <br><br>
-          <input type="password" maxlength="8" id="pwd1" name="pwd1" placeholder="Confirm password">
+          <input type="password" class="fields" maxlength="50" id="pwd1" name="pwd1" placeholder="Confirm password">
           <br><br>
 
-          <input type="button" value="Save" >
-          <br><br>
+          <input type="button" value="Save" class="save-button" onClick="save()">
+          <br><br><br />
 
           <label for="delete">Delete account:   </label>
-          <input type="button" value="Delete" >
+          <input type="button" value="Delete" class="delete-button" onClick="delete()">
        </form>
+    </div>
         
     </div>
 
