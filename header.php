feb79c46c9b56c70e40997f57dea853c7d2d2db7
nethu99 <it20632700@my.sliit.lk>
2021-05-31 15:17:54 +0530

    add pricing link to the header

--- header.php
+++ header.php
@@ -19,11 +19,14 @@
           <li class="nav-item"><a href="index.php">Home</a></li>
           <li class="nav-item"><a href="#">Games</a></li>
           <li class="nav-item"><a href="#">Subjects</a></li>
+          
+         
+          
 
           <?php 
             if (isset($_SESSION["memberID"])) {
               if ($_SESSION["TID"] == 2){
-                echo '<li class="nav-item"><a href="#">Pricing</a></li>';
+                echo '<li class="nav-item">'.'<a href="pricing.php">Pricing</a>'.'</li>';
               }
               echo '<li class="nav-item"><a href="includes/logout.inc.php">Log Out</a></li>';
               echo '<li class="nav-item-profile">';
@@ -39,7 +42,7 @@
               echo '</li>';
             }
             else {
-              echo '<li class="nav-item"><a href="#">Pricing</a></li>';
+              echo '<li class="nav-item"><a href="pricing.php">Pricing</a></li>';
               echo '<li class="nav-item-signin"><a href="login.php">Sign In </a></li>';
               echo '<li class="nav-item-signup"><a href="signup.php">Sign Up </a></li>';
             }
