f5c72eecc5e66a06f661c75c9cab6d596509a3e2
it20609962 <it20609962@my.sliit.lk>
2021-06-16 00:53:43 +0530

    contact.inc.php

--- includes/contact.inc.php
+++ includes/contact.inc.php
@@ -1,6 +1,12 @@
 <?php
     if (isset($_POST["submit"])){
 
+        $email = $_POST["email"];
+        $name = $_POST["name"]; 
+        $subject = $_POST["subject"];
+        $description = $_POST["description"];
+
+        $sql = "INSERT INTO contact(email, name, subject, description) VALUES($email, $name, $subject, $description)"
     }
     else{
         header("location: ../contact us.php");
