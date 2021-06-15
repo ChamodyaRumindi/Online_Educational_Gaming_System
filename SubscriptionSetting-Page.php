d4d7ed262baef47cdb14e167a0581302375de864
nethu99 <tharukatharu16@gmail.com>
2021-06-15 20:11:31 +0530

    remove unwanted styles

--- SubscriptionSetting-Page.php
+++ SubscriptionSetting-Page.php
@@ -1,4 +1,4 @@
-<link rel="stylesheet" href="css/SubscriptionSetting -Page.css" />
+<link rel="stylesheet" href="css/SubscriptionSetting-Page.css" />
 <link rel="stylesheet" href="css/sidebar.css" />
 
 <?php
@@ -15,32 +15,21 @@
     ?>
   </div>
 
-  
-  
 
   <div class="sub-div2">
 
-  <div class="line-break"></div>
+      <div class="sub-divContainer">
+      <h3>Your Subscription : Premium Membership</h3>
+      <h3>Plan              : Monthly</h3>
+      <h3>Renewal Date      : 21/03/2021</h3>
 
-
-  <div class="sub-divContainer">
-  <h3>Your Subscription : Premium Membership</h3>
-  <h3>Plan              : Monthly</h3>
-  <h3>Renewal Date      : 21/03/2021</h3>
-
-  <div class="button-div">
-    <button type="submit" name="RenewNow"  class="RenewNow-button" onclick="return confirm('Are you sure? Renew Now!')">Renew Now</button>
-    <button type="submit" name="CancelSubscription"  class="CancelSubscription-button" onclick="return confirm('Are you sure? Cancel Subscription!')" >Cancel Subscription</button>
-  </div>
-  </div>
-
-
+      <div class="button-div">
+        <button type="submit" name="RenewNow"  class="RenewNow-button" onclick="return confirm('Are you sure? Renew Now!')">Renew Now</button>
+        <button type="submit" name="CancelSubscription"  class="CancelSubscription-button" onclick="return confirm('Are you sure? Cancel Subscription!')" >Cancel Subscription</button>
+      </div>
+      </div>
 
   </div>
-
-
-
-
 
 </div>
 
